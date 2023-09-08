059d0865bf9d1ba8e3f65f529d3cde7530450eee
Chris Vithaya <Chrisvithaya@gmail.com>
2023-09-08 16:25:27 -0700

    pushing up codes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,12 @@
-// API Key
+// // API Key
 var apiKey = 'e5f8c29eda1bbcc8fecf3d7df7662e65'
 
 var searchBtnEl = document.querySelector('.search-button');
 var citiesListEl = document.querySelector(".cities-list");
 var inputEl = document.querySelector('.input');
+
+var cityName = localStorage.getItem('cityNameStore');
+
+var URLWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + '&units=imperial' + apiKey;
+var URLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + '&units=imperial' + apiKey;
 
