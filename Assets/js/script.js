a8b8109339802ad0fd2b0f2fca8112c120afa711
Chris Vithaya <Chrisvithaya@gmail.com>
2023-09-08 15:43:05 -0700

    updating HTML and script

---
+++ Assets/js/script.js
@@ -0,0 +1,7 @@
+// API Key
+var apiKey = 'e5f8c29eda1bbcc8fecf3d7df7662e65'
+
+var searchBtnEl = document.querySelector('.search-button');
+var citiesListEl = document.querySelector(".cities-list");
+var inputEl = document.querySelector('.input');
+
