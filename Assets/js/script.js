4f2abe1003748fbc66d8c60fce3f5fdf5c8220c8
Chris Vithaya <Chrisvithaya@gmail.com>
2023-09-08 19:17:46 -0700

    Changed codes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,16 @@
-// // API Key
+$(document).ready(function () {
+
 var apiKey = 'e5f8c29eda1bbcc8fecf3d7df7662e65'
 
-var searchBtnEl = document.querySelector('.search-button');
-var citiesListEl = document.querySelector(".cities-list");
-var inputEl = document.querySelector('.input');
+var cityEl = $('h2#city');
+var dateEl = $('h3#date');
+var weatherIconEl = $('img#weather-icon');
+var temperatureEl = $('span#temperature');
+var humidityEl = $('span#humidity');
+var windEl = $('span#wind');
+var uvIndexEl = $('span#uv-index');
+var cityListEl = $('div.cityList');
 
-var cityName = localStorage.getItem('cityNameStore');
+var cityInput = $('#city-input');
 
-var URLWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + '&units=imperial' + apiKey;
-var URLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + '&units=imperial' + apiKey;
-
+let previousCities = [];
